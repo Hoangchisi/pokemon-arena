6ada37d88cb46f2d79e19e0b4a8b3df001941cff
Hoangchisi <chisi482004@gmail.com>
2025-12-16 20:19:02 +0700

    try fix bug db

--- src/lib/db.ts
+++ src/lib/db.ts
@@ -1,13 +1,12 @@
 import { PrismaClient } from "@prisma/client";
 
-const globalForPrisma = globalThis as unknown as {
-  prisma: PrismaClient | undefined;
-};
+// Mở rộng global object để TypeScript không báo lỗi
+const globalForPrisma = global as unknown as { prisma: PrismaClient };
 
 export const prisma =
-  globalForPrisma.prisma ??
+  globalForPrisma.prisma ||
   new PrismaClient({
-    log: ["query"], // Có thể bỏ dòng này nếu không muốn log rác khi build
+    log: ["query"], // Bật log để xem câu lệnh SQL chạy thế nào (rất hữu ích khi debug)
   });
 
 if (process.env.NODE_ENV !== "production") globalForPrisma.prisma = prisma;
